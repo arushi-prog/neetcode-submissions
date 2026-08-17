e501b917e54fe07a9c2539a53d3ffaf356f63c99
Arushi Kapur <150845807+arushi-prog@users.noreply.github.com>
2026-08-17 16:39:16 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,107 @@
+// USING 2 STACKS
+// class MinStack {
+// public:
+//     stack<int> st;
+//     stack<int> minSt;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//         if(minSt.empty() || val <= minSt.top()){
+//             minSt.push(val);
+//         }
+//     }
+    
+//     void pop() {
+//         if(st.top() == minSt.top()){
+//             minSt.pop();
+//         }
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         return minSt.top();
+//     }
+// };
+
+// USING 1 STACK AND STORING VALUES IN PAIRS
+// class MinStack {
+// public:
+//     stack<pair<int,int>> st;
+
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if(st.empty()){
+//             st.push({val,val});
+//         }
+//         else{
+//             int minimum = min(val,st.top().second);
+//             st.push({val,minimum});
+//         }
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
+
+// USING ENCODING APPROACH
+class MinStack {
+public:
+    stack<long long> st;
+    long long minValue;
+
+    MinStack() {
+    }
+
+    void push(int val) {
+        if(st.empty()) {
+            st.push(val);
+            minValue = val;
+        }
+        else if(val >= minValue) {
+            st.push(val);
+        }
+        else {
+            st.push(2LL * val - minValue);
+            minValue = val;
+        }
+    }
+
+    void pop() {
+        if(st.top() < minValue) {
+            minValue = 2 * minValue - st.top();
+        }
+
+        st.pop();
+    }
+
+    int top() {
+        if(st.top() < minValue) {
+            return minValue;
+        }
+
+        return st.top();
+    }
+
+    int getMin() {
+        return minValue;
+    }
+};
