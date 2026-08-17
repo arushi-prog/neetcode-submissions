3dcbee71771a723ed4a475ce59aa376ebb01ed18
Arushi Kapur <150845807+arushi-prog@users.noreply.github.com>
2026-08-17 16:33:29 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,81 @@
+// USING 2 STACKS
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> minSt;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minSt.empty() || val <= minSt.top()){
+            minSt.push(val);
+        }
+    }
+    
+    void pop() {
+        if(st.top() == minSt.top()){
+            minSt.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
+
+
+// class MinStack {
+// public:
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+        
+//     }
+    
+//     void pop() {
+        
+//     }
+    
+//     int top() {
+        
+//     }
+    
+//     int getMin() {
+        
+//     }
+// };
+
+
+
+// class MinStack {
+// public:
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+        
+//     }
+    
+//     void pop() {
+        
+//     }
+    
+//     int top() {
+        
+//     }
+    
+//     int getMin() {
+        
+//     }
+// };
+
